9b3fb9fb110458834af3ff9829485b518c6e4c43
exbatek <exbatek@gmail.com>
2024-02-24 22:18:34 +0500

    Добавляем проверку на существование базы с миграциями

--- install.js
+++ install.js
@@ -1,10 +1,22 @@
-import { Database } from "bun:sqlite";
+import { Database } from "bun:sqlite"
+import { cOk, cWarn } from './lib/console'
 
-const db = Database.open("./index.sqlite");
+const dbFile = Bun.file("./index.sqlite")
+if (dbFile.size === 0) {
+   const db = Database.open('./index.sqlite')
 
-const query = db.query(`
-    CREATE TABLE migrations("id" integer primary key, "migration" varchar(255));
-`);
-query.run();
+   const query = db.query(`
+      CREATE TABLE migrations
+      (
+         "id"        integer primary key,
+         "migration" varchar(255)
+      );
+   `)
 
-db.close();
+   query.run()
+
+   db.close()
+   cOk('Migration db was created!')
+} else {
+   cWarn('Migration db was already create!')
+}
