2fffe4259dd5ca9eacbe6434634825cdfe9d740a
ibadin <exbatek@gmail.com>
2025-06-27 15:32:07 +0500

    Исправляем баг с неправильным получением дня по календарю вместо порядкового дня в неделе

--- migrate_create.js
+++ migrate_create.js
@@ -3,7 +3,7 @@ process.stdout.write(prompt);
 for await (const line of console) {
   const date = new Date()
   const addZero = (value) => value <= 9 ? `0${value}`: `${value}`
-  const fileName = `${date.getUTCFullYear()}_${addZero(date.getUTCMonth()+1)}_${addZero(date.getUTCDay())}_${addZero(date.getUTCHours())}${addZero(date.getUTCMinutes())}${addZero(date.getUTCSeconds())}_${line}`
+  const fileName = `${date.getUTCFullYear()}_${addZero(date.getUTCMonth()+1)}_${addZero(date.getUTCDate())}_${addZero(date.getUTCHours())}${addZero(date.getUTCMinutes())}${addZero(date.getUTCSeconds())}_${line}`
   const input = Bun.file(__dirname + "/stubs/migration.stub.js")
   const output = Bun.file(__dirname + `/migrations/${fileName}.js`)
   await Bun.write(output, input)
