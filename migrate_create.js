cc405bf28aecc44a4c520ff5fbe96bfa2dfcfc01
ibadin <exbatek@gmail.com>
2025-06-27 15:31:09 +0500

    Переделываем даты в аналог в UTC варианте

--- migrate_create.js
+++ migrate_create.js
@@ -3,9 +3,9 @@ process.stdout.write(prompt);
 for await (const line of console) {
   const date = new Date()
   const addZero = (value) => value <= 9 ? `0${value}`: `${value}`
-  const fileName = `${date.getFullYear()}_${addZero(date.getMonth()+1)}_${addZero(date.getDay())}_${addZero(date.getHours())}${addZero(date.getMinutes())}${addZero(date.getSeconds())}_${line}`
-  const input = Bun.file("./stubs/migration.stub.js")
-  const output = Bun.file(`./migrations/${fileName}.js`)
+  const fileName = `${date.getUTCFullYear()}_${addZero(date.getUTCMonth()+1)}_${addZero(date.getUTCDay())}_${addZero(date.getUTCHours())}${addZero(date.getUTCMinutes())}${addZero(date.getUTCSeconds())}_${line}`
+  const input = Bun.file(__dirname + "/stubs/migration.stub.js")
+  const output = Bun.file(__dirname + `/migrations/${fileName}.js`)
   await Bun.write(output, input)
   break;
 }
